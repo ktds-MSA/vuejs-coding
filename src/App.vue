aa249fa1e1446bf303684f99378dcdc4dc590dad
feel <thinkhero@gmail.com>
2020-06-03 22:45:06 +0900

    git ignore added

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Webstore />
+  </div>
+</template>
+
+<script>
+import Webstore from './components/Webstore.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Webstore
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
